2c2ea29a9144f7046674e8e078030cada01f49cf
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2022-05-25 09:27:14 -0500

    Fix.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
-class MyWidget extends StatelessWidget {
-  const MyWidget({
-    super.key,
-    required this.title,
-    required this.message,
-  });
 
-  final String title;
-  final String message;
 class MyApp extends StatelessWidget {
   @override
-  
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
