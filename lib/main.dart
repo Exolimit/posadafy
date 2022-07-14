b56507ff994dbf575a94866b29d05b08bfa29803
Juan Cevallos <cevallosjuanfrancisco@gmail.com>
2022-07-14 09:23:49 -0500

    :wrench: Firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,19 @@ import 'package:provider/provider.dart';
 import 'package:take_a_break/provider/providers.dart';
 import 'package:take_a_break/routes/routes.dart';
 import 'package:take_a_break/themes/theme.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(const MyApp());
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
